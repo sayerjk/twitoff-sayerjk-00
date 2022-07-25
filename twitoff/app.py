1ef90edce518e9a6ee19825bb9f7354de81efb85
sayerjk <sayerjk@gmail.com>
2022-07-25 10:47:25 -0700

    initial cleanup and dir moved

--- twitoff/app.py
+++ twitoff/app.py
@@ -4,77 +4,55 @@ from twitoff.predict import predict_user
 from .models import DB, User, Tweet
 from .twitter import add_or_update_user, vectorize_tweet
 
+
 def create_app():
 
     app = Flask(__name__)
-
-    # Tell our app where to find our database
-    # "registering" our database with app
     app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     DB.init_app(app)
-
-#  ===================================================================
 
     @app.route("/")
     def home():
 
         users = User.query.all()
-
         print(users, flush=True)
 
         return render_template('base.html', title="home", users=users)
 
     @app.route("/another")
     def another():
-        return render_template('base.html', title='another')
 
-#  ===================================================================
+        return render_template('base.html', title='another')
 
     @app.route('/reset')
     def reset():
 
-        # drop DB tables
         DB.drop_all()
-        # creates tables according to classes in models.py
         DB.create_all()
-
         tweet1_vector = vectorize_tweet('Wow...')
-        tweet2_vector = vectorize_tweet('Woah...')
-
         sayer = User(id=1, username='sayer')
-        neo = User(id=2, username='neo')
-
         tweet1 = Tweet(
             id=1, text='Wow...', 
-            user=sayer, vect=tweet1_vector)        
-        tweet2 = Tweet(
-            id=2, text='Woah...',
-            user=neo, vect=tweet2_vector)
-
+            user=sayer, vect=tweet1_vector)
         DB.session.add(sayer)
-        DB.session.add(neo)
         DB.session.add(tweet1)
-        DB.session.add(tweet2)
         DB.session.commit()
 
         return render_template('base.html', title='Reset DB')
 
-#  ===================================================================
-
     @app.route('/update')
     def update():
-        usernames = [user.username for user in User.query.all()]
+
+        usernames = [usr.username for usr in User.query.all()]
         for username in usernames:
             add_or_update_user(username)
         return render_template('base.html', title='Update')
 
-#  ===================================================================
-  
     @app.route('/user', methods=['POST'])
     @app.route('/user/<username>', methods=['GET'])
     def user(username=None, message=''):
+
         if request.method == 'GET':
             tweets = User.query.filter(User.username==username).one().tweets
 
@@ -95,13 +73,11 @@ def create_app():
             message=message
             )
 
-#  ===================================================================
-
     @app.route('/compare', methods=['POST'])
     def compare():
+
         user0 = request.values['user0']
         user1 = request.values['user1']
-
         if user0 == user1:
             message = 'Cannot compare a user to themselves'
         else:
